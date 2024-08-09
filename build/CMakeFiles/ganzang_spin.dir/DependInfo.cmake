c46d2d6d6566d38b29051a42d55539400c230624
namgabin <84686257+namgabin@users.noreply.github.com>
2024-08-09 21:31:12 +0900

    Add files via upload

---
+++ build/CMakeFiles/ganzang_spin.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gb/ganzang01/ganzang_spin.cc" "/home/gb/ganzang01/build/CMakeFiles/ganzang_spin.dir/ganzang_spin.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_ATOMIC_DYN_LINK"
+  "BOOST_DATE_TIME_DYN_LINK"
+  "BOOST_TEST_DYN_LINK"
+  "BOOST_THREAD_DYN_LINK"
+  "LIBBULLET_VERSION=2.88"
+  "LIBBULLET_VERSION_GT_282"
+  "ganzang_spin_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/lib/x86_64-linux-gnu/pkgconfig/../../../include/gazebo-11"
+  "/usr/lib/x86_64-linux-gnu/pkgconfig/../../../include/sdformat-9.10"
+  "/usr/lib/x86_64-linux-gnu/pkgconfig/../../../include/ignition/fuel_tools4"
+  "/usr/lib/x86_64-linux-gnu/pkgconfig/../../../include/ignition/common3"
+  "/usr/lib/x86_64-linux-gnu/pkgconfig/../../../include/ignition/transport8"
+  "/usr/include/pgm-5.2"
+  "/usr/lib/x86_64-linux-gnu/pkgconfig/../../../include/ignition/msgs5"
+  "/usr/lib/x86_64-linux-gnu/pkgconfig/../../../include/ignition/math6"
+  "/usr/lib/x86_64-linux-gnu/pkgconfig/../../../include/ignition/cmake2"
+  "/usr/include/uuid"
+  "/usr/include/ignition/fuel_tools4"
+  "/usr/include/ignition/common3"
+  "/usr/include/ignition/msgs5"
+  "/usr/include/ignition/math6"
+  "/usr/include/ignition/transport8"
+  "/usr/include/ignition/cmake2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ganzang_spin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ganzang_spin.dir/ganzang_spin.cc.o"
+  "libganzang_spin.pdb"
+  "libganzang_spin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ganzang_spin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
